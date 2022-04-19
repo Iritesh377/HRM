ca58a767b58e33d70066151b22559f4fb259f89b
Ritesh Sapkota <ritxgamingofficial@gmail.com>
2022-04-19 23:10:50 +0545

    managed 404 page

--- 404.php
+++ 404.php
@@ -2,6 +2,8 @@
 <title>Page Not Found</title>
 <head>
 <link rel="stylesheet" type="text/css" href="css/404.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+
 <link href='https://fonts.googleapis.com/css?family=Roboto:700' rel='stylesheet' type='text/css'>
 
 </head>
@@ -10,6 +12,8 @@
 <p class="mega">4<span class="boom">0</span>4<div class="bola">
 </div></p>
 <p class="mini">That's an error.</p>
+<div class=home><a href="index.php" class="btn btn-primary" >Go TO Home</a>
+</div>
 <script src="js/404.js"></script>
 </body>
 </html>
