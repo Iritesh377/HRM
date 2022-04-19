e0ecf68e4defc629e77b0a95b732142713c2e43d
Ritesh Sapkota <ritxgamingofficial@gmail.com>
2022-04-19 22:59:54 +0545

    added 404 page

---
+++ 404.php
@@ -0,0 +1,15 @@
+<html>
+<title>Page Not Found</title>
+<head>
+<link rel="stylesheet" type="text/css" href="css/404.css">
+<link href='https://fonts.googleapis.com/css?family=Roboto:700' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+
+<p class="mega">4<span class="boom">0</span>4<div class="bola">
+</div></p>
+<p class="mini">That's an error.</p>
+<script src="js/404.js"></script>
+</body>
+</html>
